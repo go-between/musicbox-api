1d4b93014d7c909bbbac34ddb30581b7596923e1
Dan Lavin <daniel.e.lavin@gmail.com>
2019-02-18 14:15:17 -0600

    adds room resource, associates to user

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,20 @@
+class RoomsController < ApplicationController
+  # Mark this as a JSONAPI controller, associating with the given resource
+  jsonapi resource: RoomResource
+
+  # Reference a strong resource payload defined in
+  # config/initializers/strong_resources.rb
+  strong_resource :room
+  # Run strong parameter validation for these actions.
+  # Invalid keys will be dropped.
+  # Invalid value types will log or raise based on the configuration
+  # ActionController::Parameters.action_on_invalid_parameters
+  # before_action :apply_strong_params, only: %i[create update]
+
+  def show
+    scope = jsonapi_scope(Room.where(id: params[:id]))
+    instance = scope.resolve.first
+    raise JsonapiCompliable::Errors::RecordNotFound unless instance
+    render_jsonapi(instance, scope: false)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
     scope path: '/v1' do
       resources :users
+      resources :rooms, only: [:show]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
