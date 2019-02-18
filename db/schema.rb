c722977e467930c975224e9eff4bb6fc2b1e8d9a
Dan Lavin <daniel.e.lavin@gmail.com>
2019-02-18 14:14:16 -0600

    associates user to a room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_193526) do
+ActiveRecord::Schema.define(version: 2019_02_18_194925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_193526) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "room_id"
     t.index ["email"], name: "index_users_on_email"
   end
 
