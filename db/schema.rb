3d49f95f7ae005bd9c4f6234d9e9c22c83b8ebe3
Dan Lavin <daniel.e.lavin@gmail.com>
2019-02-18 14:43:12 -0600

    adds song table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_194925) do
+ActiveRecord::Schema.define(version: 2019_02_18_202341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2019_02_18_194925) do
 
   create_table "rooms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.uuid "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
