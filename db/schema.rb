572318604d6bd633e0a9c61b82dc4452e7e614ac
Dan Lavin <daniel.e.lavin@gmail.com>
2019-03-19 08:43:15 -0500

    song and user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_135026) do
+ActiveRecord::Schema.define(version: 2019_03_19_133140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "playlists", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.uuid "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rooms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
@@ -29,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_03_05_135026) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "duration_in_seconds"
+  end
+
+  create_table "songs_users", id: false, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.uuid "song_id", null: false
+    t.index ["song_id", "user_id"], name: "index_songs_users_on_song_id_and_user_id"
+    t.index ["user_id", "song_id"], name: "index_songs_users_on_user_id_and_song_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
