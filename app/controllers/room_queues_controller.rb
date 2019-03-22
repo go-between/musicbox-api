a46ee7c796f9bf1b94e04616eb995789be0a050e
Truman Shuck <hello@trumanshuck.com>
2019-03-21 23:05:42 -0500

    [feat ] Broadcast song list when new song enqueued

--- app/controllers/room_queues_controller.rb
+++ app/controllers/room_queues_controller.rb
@@ -15,9 +15,23 @@ class RoomQueuesController < ApplicationController
     room_queue, success = jsonapi_create.to_a
 
     if success
+      ActionCable.server.broadcast('queue', serialize(enqueued_songs(room_queue)))
+      ActionCable.server.broadcast('now_playing', serialize(enqueued_songs(room_queue).first))
       render_jsonapi(room_queue, scope: false)
     else
       render_errors_for(room_queue)
     end
+  end
+
+  private
+
+  def enqueued_songs(room_queue)
+    room_queue.room.enqueued_songs
+  end
+
+  def serialize(songs)
+    JSONAPI::Serializable::Renderer
+      .new
+      .render(songs, class: { Song: SerializableSong })
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,5 @@
 class Room < ApplicationRecord
   has_many :users
+  has_many :enqueues, foreign_key: :room_id, class_name: "RoomQueue"
+  has_many :enqueued_songs, through: :enqueues, source: :song
 end
