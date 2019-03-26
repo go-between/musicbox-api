8a15baca0e247bc59625f03bbb3ae0ebda4e1c84
Dan Lavin <daniel.e.lavin@gmail.com>
2019-03-26 08:36:21 -0500

    updates tests

--- spec/graphql/songs_spec.rb
+++ spec/graphql/songs_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Songs", type: :request do
           song {
             id
             description
-            duration_in_seconds
+            durationInSeconds
             name
             youtubeId
           }
@@ -26,16 +26,22 @@ RSpec.describe "Songs", type: :request do
 
   describe "#create" do
     it "can be created" do
+      video = OpenStruct.new(duration: 1500, title: "a title", description: "a description")
+      expect(Yt::Video).to receive(:new).with(id: "an-id").and_return(video)
       post('/api/v1/graphql', params: { query: query(youtube_id: "an-id") })
       data = json_body.dig(:data, :createSong)
       id = data.dig(:song, :id)
 
-      expect(Song.exists?(id)).to eq(true)
+      song = Song.find(id)
+      expect(song.name).to eq('a title')
+      expect(song.description).to eq('a description')
+      expect(song.duration_in_seconds).to eq(1500)
       expect(data[:errors]).to be_blank
     end
 
     it "allows find-or-create by youtube_id" do
       song = create(:song, youtube_id: "the-youtube-id")
+      expect(Yt::Video).to_not receive(:new)
 
       expect do
         post('/api/v1/graphql', params: { query: query(youtube_id: "the-youtube-id") })
@@ -50,6 +56,7 @@ RSpec.describe "Songs", type: :request do
 
   context "when missing required attributes" do
     it "fails to persist when youtube_id is not specified" do
+      expect(Yt::Video).to_not receive(:new)
       expect do
         post('/api/v1/graphql', params: { query: query(youtube_id: nil) })
         data = json_body.dig(:data, :createSong)
