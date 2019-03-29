7ab5e26e3a40d464677a7ecdc3934c505f86847b
Truman Shuck <hello@trumanshuck.com>
2019-03-29 00:21:48 -0500

    [migrt] Recreate users table from devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_051721) do
+ActiveRecord::Schema.define(version: 2019_03_29_051901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -91,6 +91,18 @@ ActiveRecord::Schema.define(version: 2019_03_29_051721) do
     t.uuid "song_id", null: false
     t.index ["song_id", "user_id"], name: "index_songs_users_on_song_id_and_user_id"
     t.index ["user_id", "song_id"], name: "index_songs_users_on_user_id_and_song_id"
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
