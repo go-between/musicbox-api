3ed9aa88c684c45adb579f1ba899463829d62262
Truman Shuck <hello@trumanshuck.com>
2019-03-29 00:18:05 -0500

    [migrt] Drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_051317) do
+ActiveRecord::Schema.define(version: 2019_03_29_051721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -91,15 +91,6 @@ ActiveRecord::Schema.define(version: 2019_03_29_051317) do
     t.uuid "song_id", null: false
     t.index ["song_id", "user_id"], name: "index_songs_users_on_song_id_and_user_id"
     t.index ["user_id", "song_id"], name: "index_songs_users_on_user_id_and_song_id"
-  end
-
-  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "email"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.uuid "room_id"
-    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
