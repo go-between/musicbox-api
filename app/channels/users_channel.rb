17fa8c0b4a95267a6021964f8305d12037d868f1
Truman Shuck <hello@trumanshuck.com>
2019-04-12 22:42:51 -0500

    [feat ] Handle user disconnect

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -3,4 +3,10 @@ class UsersChannel < ApplicationCable::Channel
     room = Room.find(params[:room_id])
     stream_for room
   end
+
+  def unsubscribed
+    room_id = current_user.room_id
+    current_user.update!(room: nil)
+    BroadcastUsersWorker.perform_async(room_id)
+  end
 end
