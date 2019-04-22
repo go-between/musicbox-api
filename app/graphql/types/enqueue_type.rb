c3dbd5f4e24f89d18e638847975af13de4e81fc6
Truman Shuck <truman@hatchloyalty.com>
2019-04-22 15:12:44 -0500

    [feat ] Allow room song to default for_user to false

--- app/graphql/types/enqueue_type.rb
+++ app/graphql/types/enqueue_type.rb
@@ -2,7 +2,7 @@ module Types
   class EnqueueType < Types::BaseObject
     graphql_name 'Enqueue'
 
-    field :order, Int, null: false
+    field :id, ID, null: false
     field :song, Types::SongType, null: false
     field :user, Types::UserType, null: false
     field :room, Types::RoomType, null: false
