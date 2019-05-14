3ecb4bd4a02f33ba25c958cf98b5ff5d364c7502
Dan Lavin <daniel.e.lavin@gmail.com>
2019-05-14 09:08:11 -0500

    WIP adds mutations for removing songs from the users library

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,5 +5,6 @@ module Types
     field :join_room, mutation: Mutations::JoinRoom
     field :order_room_songs, mutation: Mutations::OrderRoomSongs
     field :delete_room_song, mutation: Mutations::DeleteRoomSong
+    field :delete_song_user, mutation: Mutations::DeleteSongUser
   end
 end
