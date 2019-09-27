6290633c8c88700fa9e4b3b56e3a4c92807f2419
Truman Shuck <hello@trumanshuck.com>
2019-09-27 18:48:31 -0500

    Rename UserLibrarySong to UserLibraryRecord

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_233126) do
+ActiveRecord::Schema.define(version: 2019_09_27_234716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -89,13 +89,13 @@ ActiveRecord::Schema.define(version: 2019_09_27_233126) do
     t.index ["youtube_id"], name: "index_songs_on_youtube_id"
   end
 
-  create_table "user_library_songs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "user_library_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "song_id"
     t.uuid "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["song_id"], name: "index_user_library_songs_on_song_id"
-    t.index ["user_id"], name: "index_user_library_songs_on_user_id"
+    t.index ["song_id"], name: "index_user_library_records_on_song_id"
+    t.index ["user_id"], name: "index_user_library_records_on_user_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
