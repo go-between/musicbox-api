90474a08d8c771ebc86a4ee7f42884fdb2eae276
Truman Shuck <hello@trumanshuck.com>
2019-09-27 18:59:24 -0500

    Update model associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "can have many songs" do
-    user = create(:user)
-    song1 = create(:song)
-    song2 = create(:song)
-    user.songs << song1
-    user.songs << song2
+  let(:user) { User.create!(email: "test@test.com", password: "123456") }
 
-    expect(user.reload.songs).to match_array([song1, song2])
+  describe "relationships" do
+    it "has many library records" do
+      record1 = create(:user_library_record, user: user)
+      record2 = create(:user_library_record, user: user)
+      record3 = create(:user_library_record, user: user)
+
+      expect(user.reload.user_library_records).to match_array([record1, record2, record3])
+    end
+
+    it "has many songs" do
+      song1 = create(:song)
+      song2 = create(:song)
+
+      record1 = create(:user_library_record, user: user, song: song1)
+      record2 = create(:user_library_record, user: user, song: song2)
+      record3 = create(:user_library_record, user: user, song: song2)
+
+      expect(user.reload.songs).to match_array([song1, song2, song2])
+    end
+
+    it "may belong to a room" do
+      room = create(:room)
+      user.update!(room: room)
+
+      expect(user.room).to eq(room)
+    end
   end
 end
