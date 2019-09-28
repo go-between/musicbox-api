6e6a711e4c487bc72c4f33f5c3b2a317c53e34d5
Truman Shuck <truman@hatchloyalty.com>
2019-09-28 15:08:58 -0500

    Update current record relationships on Room

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,6 @@ class Room < ApplicationRecord
   has_many :users
   has_many :room_playlist_records
   has_many :songs, through: :room_playlist_records
-  belongs_to :current_song, foreign_key: :current_song_id, class_name: "Song", optional: true
+  belongs_to :current_record, foreign_key: :current_record_id, class_name: "RoomPlaylistRecord", optional: true
+  has_one :current_song, through: :current_record, source: :song
 end
--- spec/factories/room.rb
+++ spec/factories/room.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :room do
     name { 'Banjo Town' }
-    current_song { create(:song) }
-    current_song_start { Time.zone.now }
+    current_record { nil }
   end
 end
