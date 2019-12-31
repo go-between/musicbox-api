b6fa9e8be64e6c01ebcd66a0b253e41339468640
Truman Shuck <truman@hatchloyalty.com>
2019-12-30 21:29:09 -0600

    Remove jsonapi resources, devise stuff that we don't need

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,7 @@ Rails.application.routes.draw do
   scope path: '/api' do
     scope path: '/v1' do
       post '/graphql', to: 'graphql#execute'
-
-      devise_for :users
       use_doorkeeper
-
-      resources :users
-      resources :rooms, only: [:show]
-      resources :room_songs, only: [:create]
-      resources :songs, only: [:create]
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
