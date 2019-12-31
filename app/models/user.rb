d4689c4e406f23cc96da1a9e2bcd786a21f857c8
Truman Shuck <hello@trumanshuck.com>
2019-12-30 20:04:29 -0600

    Allow a user to belong to many teams via association

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   belongs_to :room, optional: true, foreign_key: :active_room_id
   has_many :user_library_records
   has_many :songs, through: :user_library_records
+  has_many :team_users
+  has_many :teams, through: :team_users
 end
