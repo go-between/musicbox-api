e8bd67a6317cac971de935b25bef9e54b793dd0b
Jason Sisk <jason@sisk.org>
2020-01-16 08:39:10 -0600

    Current user can create a room

---
+++ app/graphql/mutations/room_create.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Mutations
+  class RoomCreate < Mutations::BaseMutation
+    argument :name, ID, required: true
+
+    field :room, Types::RoomType, null: true
+    field :errors, [String], null: true
+
+    def resolve(name:)
+      room = Room.new(name: name)
+
+      unless room.valid?
+        return {
+                 room: nil,
+                 errors: room.errors.full_messages,
+               }
+      end
+
+      room.save!
+
+      {
+        room: room,
+        errors: [],
+      }
+    end
+  end
+end
