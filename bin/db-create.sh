6b3c7c407b21de6e7b6d2eee52284eae2ca11731
Truman Shuck <hello@trumanshuck.com>
2020-02-04 22:09:34 -0600

    Add helper scripts to run migrations against ECS

---
+++ bin/db-create.sh
@@ -0,0 +1,1 @@
+aws2 ecs run-task --cluster musicbox-cluster-staging --task-definition musicbox-app-task-staging-db-create --count 1 --launch-type FARGATE --network-configuration "awsvpcConfiguration={subnets=[REPLACE_ME,WITH_PRIVATE_SUBNETS],securityGroups=[REPLACE_ME_WITH_ECS_SECURITY_GROUP]}"
