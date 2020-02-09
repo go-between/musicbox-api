d3f89612de2e1b7e26909331bfc1860abe51665c
Truman Shuck <hello@trumanshuck.com>
2020-02-09 00:43:55 -0600

    Add worker to send an invitation via mailgun

---
+++ app/workers/email_invitation_worker.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class EmailInvitationWorker
+  class DeliveryError < StandardError; end
+
+  include Sidekiq::Worker
+  sidekiq_options queue: "email_invitations"
+
+  def perform(invitation_id)
+    invitation = Invitation.find(invitation_id)
+
+    uri = URI("https://api.mailgun.net/v3/mg.musicbox.fm/messages")
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(request(uri, invitation))
+    end
+
+    raise DeliveryError unless res.is_a?(Net::HTTPSuccess)
+  end
+
+  private
+
+  def request(uri, invitation)
+    template_variables = {
+      inviter_name: invitation.inviting_user.name,
+      team_name: invitation.team.name,
+      token: invitation.token
+    }
+
+    req = Net::HTTP::Post.new(uri)
+    req.set_form_data(
+      from: "Truman at Musicbox <truman@musicbox.fm>",
+      to: "#{invitation.name} <#{invitation.email}>",
+      subject: "Your Musicbox Invitation",
+      template: "invitation",
+      "h:X-Mailgun-Variables": JSON.generate(template_variables)
+    )
+
+    req.basic_auth("api", ENV["MAILGUN_KEY"])
+    req
+  end
+end
