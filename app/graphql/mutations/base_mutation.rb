9e41b8666fdf4215710c6779c3bb255410c151e4
Truman Shuck <hello@trumanshuck.com>
2020-02-16 22:52:10 -0600

    Pull token create into base mutation and remove expiry for now

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -7,5 +7,13 @@ module Mutations
     def current_user
       context[:current_user]
     end
+
+    def access_token_for(user_id)
+      Doorkeeper::AccessToken.create!(
+        resource_owner_id: user_id,
+        expires_in: nil,
+        scopes: ""
+      ).token
+    end
   end
 end
