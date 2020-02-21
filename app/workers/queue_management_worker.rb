3238dbdece5fbcafa2a4fa08f919773596aec1ae
Truman Shuck <hello@trumanshuck.com>
2020-02-21 08:11:49 -0600

    Include future check in room lock

--- app/workers/queue_management_worker.rb
+++ app/workers/queue_management_worker.rb
@@ -6,22 +6,26 @@ class QueueManagementWorker
 
   def perform(room_id)
     room = Room.find(room_id)
-    return if room.playing_until&.future?
 
-    update_room!(room)
-    BroadcastNowPlayingWorker.perform_async(room_id)
-    BroadcastPlaylistWorker.perform_async(room_id)
+    update_room!(room) do
+      BroadcastNowPlayingWorker.perform_async(room_id)
+      BroadcastPlaylistWorker.perform_async(room_id)
+    end
   end
 
   private
 
   def update_room!(room)
     room.with_lock do
+      return if room.playing_until&.future?
+
       next_record = RoomPlaylist.new(room.id).generate_playlist.first
-      return idle!(room) if next_record.blank?
+      return idle!(room) && yield if next_record.blank?
 
       next_record.update!(play_state: "played", played_at: Time.zone.now)
       room.update!(current_record: next_record, playing_until: playing_until(next_record))
+
+      yield
     end
   end
 
