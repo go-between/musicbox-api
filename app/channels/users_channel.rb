86d66ec1d53a9d708f37dd8bfd91e0662d141389
Truman Shuck <hello@trumanshuck.com>
2020-02-24 18:18:02 -0600

    Remove user from room when they leave

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -2,4 +2,20 @@
 
 class UsersChannel < ApplicationCable::Channel
   delegate :subscribed, to: :subscribe_for_current_user
+
+  def unsubscribed
+    return if cached_current_user.blank?
+
+    remove_from_room!
+  end
+
+  private
+
+  def remove_from_room!
+    return if cached_current_user.active_room_id.blank?
+
+    previous_room = cached_current_user.active_room_id
+    cached_current_user.update!(active_room: nil)
+    BroadcastUsersWorker.perform_async(previous_room)
+  end
 end
