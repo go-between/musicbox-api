3694c19dbcf9147fb0b620bbb217c9ddefe04804
Truman Shuck <hello@trumanshuck.com>
2020-02-24 18:17:46 -0600

    Cache current user to avoid making a bajillion user lookup calls

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -3,9 +3,20 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
     def subscribe_for_current_user
-      return reject if current_user.active_room.blank?
+      return reject if cached_current_user.active_room.blank?
 
-      stream_for current_user.active_room
+      stream_for cached_current_user.active_room
+    end
+
+    private
+
+    def cached_current_user
+      return @cached_current_user if defined? @cached_current_user
+
+      prospective_current_user = current_user
+      return if prospective_current_user.blank?
+
+      @cached_current_user = prospective_current_user
     end
   end
 end
