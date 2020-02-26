d5cf8e2a766a2d98028e885472476153dab081c0
Truman Shuck <hello@trumanshuck.com>
2020-02-26 12:24:52 -0600

    Update broadcast message worker spec to expect email

--- spec/workers/broadcast_message_worker_spec.rb
+++ spec/workers/broadcast_message_worker_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 RSpec.describe BroadcastMessageWorker, type: :worker do
   let(:created_at) { Time.zone.now }
   let(:room) { create(:room) }
-  let(:user) { create(:user, name: "Jorm") }
+  let(:user) { create(:user, name: "Jorm", email: "a@a.a") }
   let(:room_playlist_record) { create(:room_playlist_record) }
   let(:worker) { described_class.new }
 
@@ -26,7 +26,7 @@ RSpec.describe BroadcastMessageWorker, type: :worker do
         expect(data[:createdAt]).to eq(created_at.iso8601)
         expect(data.dig(:roomPlaylistRecord, :song, :name)).to eq(room_playlist_record.song.name)
         expect(data.dig(:user, :name)).to eq("Jorm")
-        expect(data.dig(:user, :name)).to eq("a@a.a")
+        expect(data.dig(:user, :email)).to eq("a@a.a")
       end)
     end
   end
