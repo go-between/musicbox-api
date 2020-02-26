2eea10c05986e00a4da96344616e9b801323dc2d
Truman Shuck <hello@trumanshuck.com>
2020-02-25 21:17:45 -0600

    Remove memoize and delegate in action cable channel/connection

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -2,7 +2,7 @@
 
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    def subscribe_for_current_user
+    def subscribed
       return reject if current_user.active_room.blank?
 
       stream_for current_user.active_room
--- app/channels/room_playlist_channel.rb
+++ app/channels/room_playlist_channel.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class RoomPlaylistChannel < ApplicationCable::Channel
-  delegate :subscribed, to: :subscribe_for_current_user
 end
