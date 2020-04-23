7aa7a35cfd8a3fc394fc5806bd18b709c38eb95a
Truman Shuck <hello@trumanshuck.com>
2020-04-23 10:27:34 -0500

    Remove rememberable field from users

---
+++ db/migrate/20200423152618_remove_remember_at_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveRememberAtFromUser < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :remember_created_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_022511) do
+ActiveRecord::Schema.define(version: 2020_04_23_152618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -184,7 +184,6 @@ ActiveRecord::Schema.define(version: 2020_04_15_022511) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
