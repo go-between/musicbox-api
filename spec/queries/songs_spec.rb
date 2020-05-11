015d27dc3cd855f30cfa8fb84d9b8effc4fe2e0d
Truman Shuck <hello@trumanshuck.com>
2020-05-11 00:23:09 -0500

    Add specs for song ordering, user library records

--- spec/queries/songs_spec.rb
+++ spec/queries/songs_spec.rb
@@ -8,11 +8,18 @@ RSpec.describe "Songs Query", type: :request do
 
   def query
     %(
-      query Songs($query: String, $tagIds: [ID!]) {
-        songs(query: $query, tagIds: $tagIds) {
+      query Songs($query: String, $tagIds: [ID!], $order: Order) {
+        songs(query: $query, tagIds: $tagIds, order: $order) {
           id
           tags {
             id
+          }
+          userLibraryRecords {
+            id
+            source
+            fromUser {
+              id
+            }
           }
         }
       }
@@ -133,6 +140,88 @@ RSpec.describe "Songs Query", type: :request do
       expect(json_body.dig(:data, :songs).size).to eq(1)
       tag_ids = json_body.dig(:data, :songs, 0, :tags).map { |t| t[:id] }
       expect(tag_ids).to match_array([tag1.id])
+    end
+  end
+
+  describe "when retrieving associated user library records" do
+    let(:song1) { create(:song, created_at: 1.day.ago, name: "CCC") }
+    let(:song2) { create(:song, created_at: 2.days.ago, name: "AAAA") }
+    let(:other_user) { create(:user, name: "the other user") }
+
+    it "only retrieves the user's own library records" do
+      record1 = UserLibraryRecord.create!(user: user, song: song1, from_user: other_user, source: "saved_from_history")
+      record2 = UserLibraryRecord.create!(user: user, song: song2)
+      UserLibraryRecord.create!(user: other_user, song: song1)
+      UserLibraryRecord.create!(user: other_user, song: song2)
+
+      graphql_request(query: query, user: user)
+
+      song1_resp = json_body.dig(:data, :songs).find { |s| s[:id] == song1.id }
+      song2_resp = json_body.dig(:data, :songs).find { |s| s[:id] == song2.id }
+
+      expect(song1_resp.dig(:userLibraryRecords).size).to eq(1)
+      record1_resp = song1_resp.dig(:userLibraryRecords).first
+      expect(record1_resp[:id]).to eq(record1.id)
+      expect(record1_resp[:source]).to eq("saved_from_history")
+      expect(record1_resp.dig(:fromUser, :id)).to eq(other_user.id)
+
+      expect(song2_resp.dig(:userLibraryRecords).size).to eq(1)
+      record2_resp = song2_resp.dig(:userLibraryRecords).first
+      expect(record2_resp[:id]).to eq(record2.id)
+      expect(record2_resp[:source]).to be_nil
+      expect(record2_resp[:fromUser]).to be_nil
+    end
+  end
+
+  describe "ordering" do
+    let(:song1) { create(:song, created_at: 1.day.ago, name: "CCC") }
+    let(:song2) { create(:song, created_at: 2.days.ago, name: "AAAA") }
+    let(:song3) { create(:song, created_at: 3.day.ago, name: "BBBB") }
+
+    it "returns songs in creation order by default" do
+      user.update!(songs: [song1, song2, song3])
+
+      graphql_request(query: query, user: user)
+      song_ids = json_body.dig(:data, :songs).map { |s| s[:id] }
+      expect(song_ids).to eq([song3.id, song2.id, song1.id])
+    end
+
+    it "returns songs in reverse creation order" do
+      user.update!(songs: [song1, song2, song3])
+
+      graphql_request(query: query, user: user, variables: { order: { field: "createdAt", direction: "desc" } })
+      song_ids = json_body.dig(:data, :songs).map { |s| s[:id] }
+      expect(song_ids).to eq([song1.id, song2.id, song3.id])
+    end
+
+    it "returns songs in name order" do
+      user.update!(songs: [song1, song2, song3])
+
+      graphql_request(query: query, user: user, variables: { order: { field: "name", direction: "asc" } })
+      song_ids = json_body.dig(:data, :songs).map { |s| s[:id] }
+      expect(song_ids).to eq([song2.id, song3.id, song1.id])
+    end
+
+    it "returns songs in reverse name order" do
+      user.update!(songs: [song1, song2, song3])
+
+      graphql_request(query: query, user: user, variables: { order: { field: "name", direction: "desc" } })
+      song_ids = json_body.dig(:data, :songs).map { |s| s[:id] }
+      expect(song_ids).to eq([song1.id, song3.id, song2.id])
+    end
+
+    it "returns an empty array when field is invalid" do
+      user.update!(songs: [song1, song2, song3])
+
+      graphql_request(
+        query: query,
+        user: user,
+        variables: { order: { field: "name; DROP TABLE songs", direction: "desc" } }
+      )
+      song_ids = json_body.dig(:data, :songs).map { |s| s[:id] }
+
+      expect(song_ids).to eq([])
+      expect(Song.all).to be_present
     end
   end
 end
