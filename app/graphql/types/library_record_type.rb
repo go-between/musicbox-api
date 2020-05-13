5cca6c6e065bde00d360299fc032cd63acc4bd23
Dan Lavin <daniel.e.lavin@gmail.com>
2020-05-12 22:05:59 -0500

    allows the react app to use the userlibraryrecord created at date

--- app/graphql/types/library_record_type.rb
+++ app/graphql/types/library_record_type.rb
@@ -6,6 +6,7 @@ module Types
 
     field :id, ID, null: false
     field :source, String, null: true
+    field :created_at, Types::DateTimeType, null: false
 
     field :from_user, Types::UserType, null: true
     field :song, Types::SongType, null: true
